771b7ce8a455f3fb825a4921bd9b94182110c02e
Vilmos Somogyi <vsomogyi@gmail.com>
2012-10-31 21:53:09 +0100

    Flexslider styling and setup, mobile and desktop styling

--- plonetheme/fourteamwork/theme_resources/javascript/script.js
+++ plonetheme/fourteamwork/theme_resources/javascript/script.js
@@ -16,51 +16,16 @@
         });
     });
 
-    // var resizer = function () {
-    //     var $scrollable = $('.scrollable'),
-    //         height = $scrollable.find('.focuss').height(),
-    //         width = $('#content').width(),
-    //         $navig = $('.browse');
-    //     $scrollable.css('height', height + 30);
-    //     $scrollable.css('width', width - 60);
-    //     $scrollable.find('.focuss-wrapper').css('height', height);
-    //     $scrollable.find('.focuss-wrapper').css('width', width - 90);
-
-    //     // move the next/prev button
-    //     $navig.css('top', (height / 2));
-    // };
-
-    // resizer();
-
-    // $(window).resize(function(){
-    //     resizer();
-    // });
-
-
-
-
-// TODO this is not working!!!!!!!!!!
-    // $.fn.handleSwipes = function() {
-    //   return this.each(function() {
-    //     var api = $(this).data("scrollable");
-
-    //     api.getRoot().addSwipeEvents()
-    //        .bind('swipeleft', function() {
-    //          api.next(100);
-    //          window.console.log('LEFT!!!!!');
-    //        })
-    //        .bind('swiperight', function() {
-    //          api.prev(100);
-    //          window.console.log('RIGHT!!!!!');
-    //        });
-    //   });
-    // };
-
-    // $('.scrollable').scrollable({touch:false}).handleSwipes();
-
-
     $('.flexslider').flexslider({
         animation: 'slide'
     });
+
+    $('.logo-flexslider').flexslider({
+        animation: 'slide',
+        direction: 'vertical',
+        controlNav: false,
+        directionNav: false
+    });
+
 
 }(jQuery));
